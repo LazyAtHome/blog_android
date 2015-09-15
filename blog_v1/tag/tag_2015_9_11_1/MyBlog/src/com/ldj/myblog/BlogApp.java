5dae3d54f4870e16455489bbdf79c8fac72d5b24
15000712803peng <15000712803@139.com>
2015-09-15 10:43:44 +0800

    delete master folder before

--- blog_v1/tag/tag_2015_9_11_1/MyBlog/src/com/ldj/myblog/BlogApp.java
+++
@@ -1,5 +0,0 @@
-package com.ldj.myblog;
-
-public class BlogApp {
-
-}
--- blog_v1/truck/MyBlog/src/com/ldj/myblog/resp/BaseResp.java
+++
@@ -1,7 +0,0 @@
-package com.ldj.myblog.resp;
-
-public class BaseResp {
-	public static final int OK = 0;
-	public static final int FAIL = Integer.MAX_VALUE;
-
-}
